43b4de1d8a70d2eaf22b20f3f07ca4908c71cc66
miltonbernhardt <miltonbernhardt@gmail.com>
2021-07-31 21:02:20 -0300

    was configured eureka

--- src/main/java/com/brikton/labapps/mspedidos/rest/DetallePedidoRest.java
+++ src/main/java/com/brikton/labapps/mspedidos/rest/DetallePedidoRest.java
@@ -3,29 +3,22 @@ package com.brikton.labapps.mspedidos.rest;
 import com.brikton.labapps.mspedidos.domain.DetallePedido;
 import com.brikton.labapps.mspedidos.exception.RecursoNoEncontradoException;
 import com.brikton.labapps.mspedidos.service.DetallePedidoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/detallepedido")
 public class DetallePedidoRest {
-    
+
     @Autowired
     DetallePedidoService service;
 
     @PostMapping()
-    public ResponseEntity<?> agregarItem(@RequestBody DetallePedido detalle, @RequestParam Integer idPedido){
+    public ResponseEntity<?> agregarItem(@RequestBody DetallePedido detalle, @RequestParam Integer idPedido) {
         try {
-            detalle = service.agregarDetalle(detalle,idPedido);
+            detalle = service.agregarDetalle(detalle, idPedido);
         } catch (RecursoNoEncontradoException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
@@ -33,9 +26,9 @@ public class DetallePedidoRest {
     }
 
     @PutMapping
-    public ResponseEntity<?> modificarDetalle(@RequestBody DetallePedido detalle){
+    public ResponseEntity<?> modificarDetalle(@RequestBody DetallePedido detalle) {
         try {
-           detalle = service.actualizarDetalle(detalle);
+            service.actualizarDetalle(detalle);
         } catch (RecursoNoEncontradoException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
@@ -43,7 +36,7 @@ public class DetallePedidoRest {
     }
 
     @DeleteMapping
-    public ResponseEntity<?> eliminarDetalle(@RequestBody DetallePedido detalle){
+    public ResponseEntity<?> eliminarDetalle(@RequestBody DetallePedido detalle) {
         try {
             service.eliminarDetalle(detalle);
         } catch (RecursoNoEncontradoException e) {
